9612e4f5246f0a9292bad43aa8e531a53697fb80
noahbetik <noah.betik@gmail.com>
2021-06-07 16:01:18 -0400

    added textfields for title and text on announcement editing

--- lanerope/lib/screens/AnnouncementEditor.dart
+++ lanerope/lib/screens/AnnouncementEditor.dart
@@ -5,7 +5,8 @@ class AnnouncementEditor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final TextEditingController title = TextEditingController();
+    final TextEditingController titleText = TextEditingController();
+    final TextEditingController mainText = TextEditingController();
 
     return Scaffold(
       appBar: AppBar(title: Text("Create an Announcement")),
@@ -13,13 +14,77 @@ class AnnouncementEditor extends StatelessWidget {
         padding: EdgeInsets.all(8.0),
         child: Column(
           children: [
-            TextFormField(
-              controller: title,
+            TextFormField( // eventually wanna replace these with app-level theme constants
+              maxLength: 100, // idk
+              controller: titleText,
               decoration: InputDecoration(
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 1.0
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 2.0
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 2.0
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
                 hintText: "Title",
                 hintStyle: TextStyle(
                   color: Colors.grey
                 )
+              ),
+            ),
+
+            SingleChildScrollView(
+              padding: EdgeInsets.zero,
+              child: TextFormField(// eventually wanna replace these with app-level theme constants
+                scrollPadding: EdgeInsets.zero,
+                maxLines: 10, // idk
+                controller: mainText,
+                decoration: InputDecoration(
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width: 1.0
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.blue,
+                          width: 2.0
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width: 2.0
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                    hintText: "Title",
+                    hintStyle: TextStyle(
+                        color: Colors.grey
+                    )
+                ),
               ),
             )
           ]
