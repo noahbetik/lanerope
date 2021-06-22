c8aff5a55aeff6e40218989d42739cf21d0689fc
noahbetik <noah.betik@gmail.com>
2021-06-22 16:41:34 -0400

    began setup for event management

--- lanerope/lib/screens/calendar.dart
+++ lanerope/lib/screens/calendar.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:lanerope/pagesDrawer.dart' as pd;
 import 'package:table_calendar/table_calendar.dart';
+import 'package:lanerope/globals.dart' as globals;
 
 bool ios = Platform.isIOS;
 bool android = Platform.isAndroid;
+Map<DateTime, List> events = {}; // gotta implement events yourself
 
 class Calendar extends StatefulWidget {
   @override
@@ -15,15 +17,21 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
-
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
+  List _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Calendar")),
+        appBar: AppBar(
+          title: Text("Calendar"),
+          actions: globals.role == "Coach/Admin" ? [IconButton(onPressed: () {}, icon: Icon(Icons.add))] : null,
+        ),
         body: TableCalendar(
           firstDay: DateTime.utc(2020, 9, 1),
           lastDay: DateTime.utc(2022, 06, 31),
@@ -46,6 +54,9 @@ class _CalendarState extends State<Calendar> {
           onPageChanged: (focusedDay) {
             // No need to call `setState()` here
             _focusedDay = focusedDay;
+          },
+          eventLoader: (day) {
+            return _getEventsForDay(day);
           },
         ),
         drawer: pd.PagesDrawer().importDrawer(context));
