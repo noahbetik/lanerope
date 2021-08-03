0ac0f8c0fd6500f60c587a4657538712ed3d602e
noahbetik <noah.betik@gmail.com>
2021-08-03 17:00:02 -0400

    minor chat UI imporovements

--- lanerope/lib/DM/MessageView.dart
+++ lanerope/lib/DM/MessageView.dart
@@ -12,6 +12,7 @@ final CollectionReference messages =
 class MessageView extends StatefulWidget {
   final String convoName;
   final String chatID;
+
   MessageView({required this.convoName, required this.chatID});
 
   @override
@@ -20,7 +21,7 @@ class MessageView extends StatefulWidget {
   }
 }
 
-class MsgViewState extends State<MessageView>{
+class MsgViewState extends State<MessageView> {
   final FocusNode _focus = new FocusNode();
   final _sCtrl = ScrollController();
   final TextEditingController msgCtrl = TextEditingController();
@@ -40,123 +41,133 @@ class MsgViewState extends State<MessageView>{
         FocusManager.instance.primaryFocus?.unfocus();
       },
       child: Scaffold(
-        appBar: AppBar(title: Text(widget.convoName)),
-        body: StreamBuilder<DocumentSnapshot>(
-          stream: messages.doc(widget.chatID).snapshots(),
-          builder: (context, snap) {
-            if (!snap.hasData) {
-              return SizedBox.shrink();
-            } else {
-              var ds = snap.data;
-              SchedulerBinding.instance?.addPostFrameCallback((_) {
-                _sCtrl.animateTo(
-                  _sCtrl.position.maxScrollExtent,
-                  duration: Duration(milliseconds: 500),
-                  curve: Curves.fastOutSlowIn,
-                );
-              });
-              return ListView.builder(
-                controller: _sCtrl,
-                  itemCount: ds!['messages'].length,
-                  itemBuilder: (context, i) {
-                    if (ds['messages'].length != 0) {
-                      List info = ds['messages'][i].split("⛄𝄞⛄");
-                      // has the potential to go buggy but makes db wayyyyyy simpler
-                      // ⛄𝄞⛄
-                      return Message(
-                          text: info[0],
-                          timestamp: info[1],
-                          user: info[2] == globals.currentUID
-                              ? Participant.you
-                              : Participant.them,
-                          chatID: widget.chatID);
-                    } else {
-                      print("hek");
-                      return SizedBox.shrink();
-                    }
-                  });
-            }
-          },
-        ),
-        bottomNavigationBar: Container(
-            padding: MediaQuery.of(context).viewInsets,
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  color: Colors.grey[200]),
-              child: Row(children: [
-                SizedBox(width: 5),
-                Material(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(100),
-                    child: InkWell(
-                      customBorder: CircleBorder(),
-                      radius: 80,
-                      onTap: () {},
-                      splashColor: Colors.lightBlueAccent.withOpacity(0.5),
-                      highlightColor: Colors.lightBlueAccent.withOpacity(0.5),
-                      child: Container(
-                        width: 42,
-                        height: 42,
-                        child: Icon(Icons.image),
-                      ),
-                    )),
-                SizedBox(width: 4),
-                Expanded(
-                    child: TextField(
-                  focusNode: _focus,
-                  controller: msgCtrl,
-                  keyboardType: TextInputType.multiline,
-                  textCapitalization: TextCapitalization.sentences,
-                  maxLines: 10,
-                  minLines: 1,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Type a message',
-                  ),
-                )),
-                _focus.hasFocus
-                    ? Material(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(100),
-                        child: InkWell(
-                          customBorder: CircleBorder(),
-                          radius: 80,
-                          onTap: () {
-                            // db update here
-                            messages
-                                .doc(widget.chatID)
-                                .update({
-                                  'messages': FieldValue.arrayUnion([
-                                    msgCtrl.text +
-                                        "⛄𝄞⛄" +
-                                        DateTime.now().toString() +
-                                        "⛄𝄞⛄" +
-                                        globals.currentUID,
-                                  ]),
-                                  'status': "sent"
-                                })
-                                .then((value) => messages
-                                    .doc(widget.chatID)
-                                    .update({'status': 'sent'}))
-                                .catchError((error) => print(error));
-                            msgCtrl.clear();
-                          },
-                          splashColor: Colors.redAccent.withOpacity(0.5),
-                          highlightColor: Colors.redAccent.withOpacity(0.5),
-                          child: Container(
-                            width: 42,
-                            height: 42,
-                            child: Icon(Icons.send_rounded),
-                          ),
-                        ))
-                    : SizedBox.shrink(),
-                SizedBox(width: 4)
-              ]),
-            )),
-      ),
+          appBar: AppBar(title: Text(widget.convoName)),
+          body: StreamBuilder<DocumentSnapshot>(
+            stream: messages.doc(widget.chatID).snapshots(),
+            builder: (context, snap) {
+              if (!snap.hasData) {
+                return SizedBox.shrink();
+              } else {
+                var ds = snap.data;
+                SchedulerBinding.instance?.addPostFrameCallback((_) {
+                  _sCtrl.animateTo(
+                    _sCtrl.position.minScrollExtent,
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.fastOutSlowIn,
+                  );
+                });
+                int num = ds!['messages'].length;
+                return ListView.builder(
+                    reverse: true,
+                    controller: _sCtrl,
+                    itemCount: num,
+                    itemBuilder: (context, i) {
+                      if (ds['messages'].length != 0) {
+                        List info = ds['messages'][num - i - 1].split("⛄𝄞⛄");
+                        // has the potential to go buggy but makes db wayyyyyy simpler
+                        // ⛄𝄞⛄
+                        return Message(
+                            text: info[0],
+                            timestamp: info[1],
+                            user: info[2] == globals.currentUID
+                                ? Participant.you
+                                : Participant.them,
+                            chatID: widget.chatID);
+                      } else {
+                        print("hek");
+                        return SizedBox.shrink();
+                      }
+                    });
+              }
+            },
+          ),
+          bottomNavigationBar: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                  padding: MediaQuery.of(context).viewInsets,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 2),
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        color: Colors.grey[200]),
+                    child: Row(children: [
+                      SizedBox(width: 5),
+                      Material(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(100),
+                          child: InkWell(
+                            customBorder: CircleBorder(),
+                            radius: 80,
+                            onTap: () {},
+                            splashColor:
+                                Colors.lightBlueAccent.withOpacity(0.5),
+                            highlightColor:
+                                Colors.lightBlueAccent.withOpacity(0.5),
+                            child: Container(
+                              width: 42,
+                              height: 42,
+                              child: Icon(Icons.image),
+                            ),
+                          )),
+                      SizedBox(width: 4),
+                      Expanded(
+                          child: TextField(
+                        focusNode: _focus,
+                        controller: msgCtrl,
+                        keyboardType: TextInputType.multiline,
+                        textCapitalization: TextCapitalization.sentences,
+                        maxLines: 10,
+                        minLines: 1,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Type a message',
+                        ),
+                      )),
+                      _focus.hasFocus
+                          ? Material(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(100),
+                              child: InkWell(
+                                customBorder: CircleBorder(),
+                                radius: 80,
+                                onTap: () {
+                                  // db update here
+                                  messages
+                                      .doc(widget.chatID)
+                                      .update({
+                                        'messages': FieldValue.arrayUnion([
+                                          msgCtrl.text +
+                                              "⛄𝄞⛄" +
+                                              DateTime.now().toString() +
+                                              "⛄𝄞⛄" +
+                                              globals.currentUID,
+                                        ]),
+                                        'status': "sent"
+                                      })
+                                      .then((value) => messages
+                                          .doc(widget.chatID)
+                                          .update({'status': 'sent'}))
+                                      .catchError((error) => print(error));
+                                  msgCtrl.clear();
+                                },
+                                splashColor: Colors.redAccent.withOpacity(0.5),
+                                highlightColor:
+                                    Colors.redAccent.withOpacity(0.5),
+                                child: Container(
+                                  width: 42,
+                                  height: 42,
+                                  child: Icon(Icons.send_rounded),
+                                ),
+                              ))
+                          : SizedBox.shrink(),
+                      SizedBox(width: 4)
+                    ]),
+                  )),
+              SizedBox(height: 4.0)
+            ],
+          )),
     );
   }
 }
