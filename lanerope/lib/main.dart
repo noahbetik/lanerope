b621da07e26a5e2d808c38f17a8d0c2e1a10af4e
noahbetik <noah.betik@gmail.com>
2021-08-09 15:47:47 -0400

    background and terminated state notis work, setup for foreground notis (currentl nonfunctional)

--- lanerope/lib/main.dart
+++ lanerope/lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:lanerope/DesignChoices.dart' as dc;
 import 'package:lanerope/globals.dart' as globals;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import './screens/home.dart';
 import "./screens/login.dart";
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'lanerope_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.max,
+);
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   print("aight lets go");
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(Lanerope());
 }
 
@@ -27,9 +47,17 @@ Future<bool> loginState() async {
 
 Future<void> lockedSubs() async {
   final prefs = await SharedPreferences.getInstance();
-  globals.subLock = prefs.getBool("boxesLocked") ?? false; // future bool is true if logged in
+  globals.subLock =
+      prefs.getBool("boxesLocked") ?? false; // future bool is true if logged in
 }
 
+Future<void> notiChannelSetup() async {
+  print("noti setup");
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+}
 
 class Lanerope extends StatefulWidget {
   // Create the initialization Future outside of `build`:
@@ -47,13 +75,14 @@ class _LaneropeState extends State<Lanerope> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   final Future<bool> _login = loginState();
   final Future<void> _subs = lockedSubs();
+  final Future<void> _notis = notiChannelSetup();
 
   @override
   Widget build(BuildContext context) {
     print("inside build widget");
     return FutureBuilder(
       // Initialize FlutterFire:
-      future: Future.wait([_initialization, _login, _subs]),
+      future: Future.wait([_initialization, _login, _subs, _notis]),
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         // Check for errors
         if (snapshot.hasError) {
@@ -77,6 +106,31 @@ class _LaneropeState extends State<Lanerope> {
             login = true;
             print("user already logged in");
           }
+          FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+            RemoteNotification? notification = message.notification;
+            AndroidNotification? android = message.notification?.android;
+            print("we got something");
+
+            // If `onMessage` is triggered with a notification, construct our own
+            // local notification to show to users using the created channel.
+            if (notification != null && android != null) {
+              print("kk we rolling");
+              flutterLocalNotificationsPlugin.show(
+                  notification.hashCode,
+                  notification.title,
+                  notification.body,
+                  NotificationDetails(
+                    android: AndroidNotificationDetails(
+                      channel.id,
+                      channel.name,
+                      channel.description,
+                      icon: android.smallIcon,
+                      // other properties...
+                    ),
+                  ));
+            }
+          });
+
           return new MaterialApp(
               debugShowCheckedModeBanner: false,
               title: "Lanerope",
